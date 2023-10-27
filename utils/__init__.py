0579dc5bee10e4a6ca7bb73821ebbf215ee2f531
Sabine Nicolle <snicolle@uci.edu>
2023-10-27 11:03:30 -0700

    small update

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
 import os
 import logging
 from hashlib import sha256
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urldefrag
 
 def get_logger(name, filename=None):
     logger = logging.getLogger(name)

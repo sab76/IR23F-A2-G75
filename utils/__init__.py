519fc678bd0fa27c3f149b507e0ed6ed4159e8d2
Sabine Nicolle <snicolle@uci.edu>
2023-10-28 06:01:07 -0700

    changed debugging

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,13 +6,13 @@ import re
 
 def get_logger(name, filename=None):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG) #logger.setLevel(logging.INFO)
+    logger.setLevel(logging.INFO) #logger.setLevel(logging.DEBUG) 
     if not os.path.exists("Logs"):
         os.makedirs("Logs")
     fh = logging.FileHandler(f"Logs/{filename if filename else name}.log")
     fh.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG) #ch.setLevel(logging.INFO)
+    ch.setLevel(logging.INFO) #ch.setLevel(logging.DEBUG) 
     formatter = logging.Formatter(
        "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     fh.setFormatter(formatter)
